b762197a5fa738de3ba4383453a6028c0f5b4c52
NamespaceSTD <ihasnamespacestd@gmail.com>
2013-03-01 21:39:38 -0500

    SDLKFj

--- columbiaconnect/urls.py
+++ columbiaconnect/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
 from columbiaconnect.views import home
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
