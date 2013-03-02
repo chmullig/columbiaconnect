272077be4f9eeaf03e9b976c28ad1ddd37917cb3
NamespaceSTD <ihasnamespacestd@gmail.com>
2013-03-02 04:09:41 -0500

    Merged with Martin's files

--- columbiaconnect/views.py
+++ columbiaconnect/views.py
@@ -13,9 +13,8 @@ import datetime
 def home(request):
 	stuff = {"connexes" : Connex.objects.order_by("name")}
 	template = 'frontend/index.html'
-	now = datetime.datetime.now()
 	if request.user.is_authenticated():
-		stuff.update({'current_date': now,'logged_in': 'YES'})
+		stuff.update({'logged_in': 'YES'})
 	else:
 		stuff.update({'logged_in': 'NO'})		
 	return render_to_response(template, stuff, context_instance=RequestContext(request))
@@ -54,22 +53,3 @@ def logout_page(request):
      response = redirect('home')
      response.delete_cookie('user_location')
      return response
-		
-def current_datetime(request):
-	now = datetime.datetime.now()
-	template = 'dateapp/current_datetime.html'
-	return render_to_response(template, {'current_date': now})
-
-def hours_ahead(request, offset):
-	try:
-		offset = int(offset)
-	except ValueError:
-		raise Http404()
-	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	
-	template = 'dateapp/hours_ahead.html'
-	return render_to_response(template, {'offset': offset, 'from_now': dt})
-	
-	
-	"""html = "<html><body> In %s hour(s), it will be %s. </body></html>" % (offset, dt)
-	return HttpResponse(html)"""
