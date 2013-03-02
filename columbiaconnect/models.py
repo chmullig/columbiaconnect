b5cd6bc260c6d93008cbead2b71be467f386d91a
Chris Mulligan <clm2186@columbia.edu>
2013-03-02 04:39:30 -0500

    Render categories properly

--- columbiaconnect/models.py
+++ columbiaconnect/models.py
@@ -12,6 +12,9 @@ class Student(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 
     class Meta:
         verbose_name_plural = "Categories"
