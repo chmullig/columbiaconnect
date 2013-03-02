bd17f38210192a3fe0d35fce95b61fad455fcc9f
NamespaceSTD <ihasnamespacestd@gmail.com>
2013-03-01 23:21:03 -0500

    some changes

--- columbiaconnect/urls.py
+++ columbiaconnect/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.STATIC_DOC_ROOT}),
+        {'document_root': settings.STATIC_ROOT}),
 )
